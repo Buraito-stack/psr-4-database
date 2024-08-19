af6606ca502e8ab3a9eafa3339061da6d49bd440
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-08-19 11:01:21 +0700

    Update Request.php

--- src/Libraries/Request.php
+++ src/Libraries/Request.php
@@ -53,55 +53,5 @@ class Request
     {
         return $this->uri;
     }
-
-    public function validate(array $data, array $rules)
-    {
-        $errors = [];
-        
-        foreach ($rules as $field => $rule) {
-            $rulesArray = explode('|', $rule);
-            foreach ($rulesArray as $r) {
-                [$ruleName, $param] = explode(':', $r) + [null, null];
-    
-                switch ($ruleName) {
-                    case 'required':
-                        if (empty(trim($data[$field] ?? ''))) {
-                            $errors[$field][] = "The $field field is required.";
-                        }
-                        break;
-    
-                    case 'string':
-                        if (!is_string($data[$field]) || empty(trim($data[$field]))) {
-                            $errors[$field][] = "The $field must be a valid string.";
-                        }
-                        break;
-                        
-                    case 'min':
-                        $min = $param ?? 3;
-                        if (strlen($data[$field]) < $min) {
-                            $errors[$field][] = "The $field must be at least $min characters.";
-                        }
-                        break;
-    
-                    case 'max':
-                        $max = $param ?? 25;
-                        if (strlen($data[$field]) > $max) {
-                            $errors[$field][] = "The $field must not be greater than $max characters.";
-                        }
-                        break;
-    
-                    case 'integer':
-                        if (!filter_var($data[$field], FILTER_VALIDATE_INT)) {
-                            $errors[$field][] = "The $field must be an integer.";
-                        }
-                        break;
-                }
-            }
-        }
-    
-        if ($errors) {
-            throw new ValidatorException("Validation failed", 0, null, $errors);
-        }
-    }
 }    
 ?>
