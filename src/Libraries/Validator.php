b3ea0491b6368c7cd1359dcaab723580ce5c73f8
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-08-21 15:52:26 +0700

    Update Validator.php

--- src/Libraries/Validator.php
+++ src/Libraries/Validator.php
@@ -6,61 +6,95 @@ use MiniMarkPlace\Exceptions\ValidatorException;
 
 class Validator
 {
-    /**
-     * Validate data who against rules.
-     *
-     * @param array $data
-     * @param array $rules
-     * @return void
-     * @throws ValidatorException
-     */
-    public static function validate(array $data, array $rules): void
+    private static $messages = [
+        'required' => ':attribute diperlukan.',
+        'string'   => ':attribute harus berupa string.',
+        'min'      => ':attribute harus memiliki minimal :min karakter.',
+        'max'      => ':attribute tidak boleh lebih dari :max karakter.',
+        'integer'  => ':attribute harus berupa integer.',
+    ];
+
+    public static function validate(array $data, array $rules)
     {
         $errors = [];
-        
-        foreach ($rules as $field => $rule) {
-            $rulesArray = explode('|', $rule);
-            foreach ($rulesArray as $r) {
-                [$ruleName, $param] = explode(':', $r) + [null, null];
-    
-                switch ($ruleName) {
-                    case 'required':
-                        if (empty(trim($data[$field] ?? ''))) {
-                            $errors[$field][] = "The $field field is required.";
-                        }
-                        break;
-    
-                    case 'string':
-                        if (!is_string($data[$field]) || empty(trim($data[$field]))) {
-                            $errors[$field][] = "The $field must be a valid string.";
-                        }
-                        break;
-                        
-                    case 'min':
-                        $min = $param ?? 3;
-                        if (strlen($data[$field]) < $min) {
-                            $errors[$field][] = "The $field must be at least $min characters.";
-                        }
-                        break;
-    
-                    case 'max':
-                        $max = $param ?? 25;
-                        if (strlen($data[$field]) > $max) {
-                            $errors[$field][] = "The $field must not be greater than $max characters.";
-                        }
-                        break;
-    
-                    case 'integer':
-                        if (!filter_var($data[$field], FILTER_VALIDATE_INT)) {
-                            $errors[$field][] = "The $field must be an integer.";
-                        }
-                        break;
+
+        foreach ($rules as $field => $ruleSet) {
+            $rulesArray = explode('|', $ruleSet);
+
+            foreach ($rulesArray as $rule) {
+                $value = $data[$field] ?? null;
+                $ruleName = explode(':', $rule)[0];
+                $parameters = explode(':', $rule);
+
+                if (method_exists(static::class, $ruleName)) {
+                    $error = static::$ruleName($field, $value, $parameters);
+
+                    if ($error) {
+                        $errors[$field][] = $error;
+                    }
                 }
             }
         }
-    
-        if ($errors) {
-            throw new ValidatorException("Validation failed", 0, null, $errors);
+
+        if (!empty($errors)) {
+            throw new ValidatorException($errors);
         }
+
+        return true;
+    }
+
+    protected static function required($field, $value, $parameters)
+    {
+        if (is_null($value) || $value === '') {
+            return str_replace(':attribute', $field, self::$messages['required']);
+        }
+
+        return null;
+    }
+
+    protected static function string($field, $value, $parameters)
+    {
+        if (!is_string($value)) {
+            return str_replace(':attribute', $field, self::$messages['string']);
+        }
+
+        return null;
+    }
+
+    protected static function min($field, $value, $parameters)
+    {
+        $min = $parameters[1] ?? 0;
+        if (strlen($value) < $min) {
+            return str_replace(
+                [':attribute', ':min'],
+                [$field, $min],
+                self::$messages['min']
+            );
+        }
+
+        return null;
+    }
+
+    protected static function max($field, $value, $parameters)
+    {
+        $max = $parameters[1] ?? PHP_INT_MAX;
+        if (strlen($value) > $max) {
+            return str_replace(
+                [':attribute', ':max'],
+                [$field, $max],
+                self::$messages['max']
+            );
+        }
+
+        return null;
+    }
+
+    protected static function integer($field, $value, $parameters)
+    {
+        if (!filter_var($value, FILTER_VALIDATE_INT)) {
+            return str_replace(':attribute', $field, self::$messages['integer']);
+        }
+
+        return null;
     }
 }
